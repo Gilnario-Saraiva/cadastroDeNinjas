f1a8943561055f607dd084c0db38dae6d0a52c60
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2026-02-01 14:07:45 -0300

    Criação da controller - aula 2.

---
+++ src/main/java/com/java10x/cadastroDeNinjas/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.java10x.cadastroDeNinjas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Bem-vindo a aplicação cadastro de ninjas.";
+    }
+}
