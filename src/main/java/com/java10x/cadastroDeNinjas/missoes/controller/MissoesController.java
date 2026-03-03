623c0a53b90cbab5bed836734407f3ff3066fe97
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2026-03-02 21:25:31 -0300

    feat: Criando rotas dos controller com mock

---
+++ src/main/java/com/java10x/cadastroDeNinjas/missoes/controller/MissoesController.java
@@ -0,0 +1,28 @@
+package com.java10x.cadastroDeNinjas.missoes.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listas")
+    public String listarMissoes(){
+        return "Missões listadas.";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso.";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão Alterada.";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missão deletada.";
+    }
+}
--- src/main/java/com/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/com/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -1,8 +1,6 @@
 package com.java10x.cadastroDeNinjas.ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,30 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Bem-vindo a aplicação cadastro de ninjas.";
+    }
+
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Todos os ninjas";
+    }
+
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPoId(){
+        return "Ninja por id";
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarNinjasPoId(){
+        return "alterarNinjasPoId";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Ninja deletado por ID";
     }
 }
