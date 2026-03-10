cebee6539f27d5f71f7673027a3755d6cc8e78d8
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2026-03-09 21:18:26 -0300

    refactor: Ajustando Services e Controllers

--- src/main/java/com/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/com/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -27,17 +27,17 @@ public class NinjaController {
     public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja){ return ninjaService.criarNinja(ninja); }
 
     @GetMapping("/listar")
-    public List<NinjaModel> listarNinjas(){
+    public List<NinjaDTO> listarNinjas(){
         return ninjaService.listarNinjas();
     }
 
     @GetMapping("/listar/{id}")
-    public NinjaModel listarNinjaPorId(@PathVariable Long id){
+    public NinjaDTO listarNinjaPorId(@PathVariable Long id){
         return ninjaService.listarNinjaPorId(id);
     }
 
     @PutMapping("/alterar/{id}")
-    public NinjaModel alterarNinjasPoId(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado){
+    public NinjaDTO alterarNinjasPoId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado){
         return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
--- src/main/java/com/java10x/cadastroDeNinjas/ninjas/service/NinjaService.java
+++ src/main/java/com/java10x/cadastroDeNinjas/ninjas/service/NinjaService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class NinjaService {
@@ -20,12 +21,17 @@ public class NinjaService {
         this.ninjaMapper = ninjaMapper;
     }
 
-    public List<NinjaModel> listarNinjas(){
-        return ninjaRepository.findAll();
+    public List<NinjaDTO> listarNinjas(){
+
+        List<NinjaModel> ninjas = ninjaRepository.findAll();
+        return ninjas.stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
     }
 
-    public NinjaModel listarNinjaPorId(Long id){
-        return ninjaRepository.findById(id).orElse(null);
+    public NinjaDTO listarNinjaPorId(Long id){
+        Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.map(ninjaMapper::map).orElse(null);
     }
 
     public NinjaDTO criarNinja(NinjaDTO ninjaDTO){
@@ -36,11 +42,14 @@ public class NinjaService {
 
     public void deletarNinjaPorId(Long id) {ninjaRepository.deleteById(id);}
 
-    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado){
-        if(ninjaRepository.existsById(id)){
-            ninjaAtualizado.setId(id);
-            return ninjaRepository.save(ninjaAtualizado);
-        }
-        return null;
+    public NinjaDTO atualizarNinja(Long id, NinjaDTO ninjaDTO){
+       Optional<NinjaModel> ninjaExistente = ninjaRepository.findById(id);
+       if(ninjaExistente.isPresent()){
+           NinjaModel ninjaAtualizado = ninjaMapper.map(ninjaDTO);
+           ninjaAtualizado.setId(id);
+           NinjaModel ninjaSalvo = ninjaRepository.save(ninjaAtualizado);
+           return ninjaMapper.map(ninjaSalvo);
+       }
+       return null;
     }
 }
