d6c3b9a747579b68eb1e251d38d7dd6bb1e2cb28
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2026-03-19 16:47:51 -0300

    Adicionando swagger

--- src/main/java/com/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/com/java10x/cadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -3,6 +3,9 @@ package com.java10x.cadastroDeNinjas.ninjas.controller;
 import com.java10x.cadastroDeNinjas.ninjas.model.NinjaDTO;
 import com.java10x.cadastroDeNinjas.ninjas.model.NinjaModel;
 import com.java10x.cadastroDeNinjas.ninjas.service.NinjaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +24,14 @@ public class NinjaController {
     }
 
     @GetMapping("/boasvindas")
+    @Operation(summary = "Mensagem de boas vindas", description = "Rota com mensagem de boas vidas para quem acessa.")
     public String boasVindas() {
         return "Bem-vindo a aplicação cadastro de ninjas.";
     }
 
     @PostMapping("/criar")
+    @Operation(summary = "Cria um novo ninja.", description = "Rota cria um novo ninja e insere no banco de dados.")
+    @ApiResponses(value = {@ApiResponse (responseCode = "201", description = "Ninja criado com sucesso.")})
     public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja) {
         NinjaDTO ninjaDTO = ninjaService.criarNinja(ninja);
         return ResponseEntity.status(HttpStatus.CREATED).body("Ninja Criado com Sucesso: " + ninjaDTO.getNome());
